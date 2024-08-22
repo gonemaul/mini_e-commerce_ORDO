4bb688d1c6fcda17f40d0ba8ce1782f767b95984
Maulana <wongdesoo366@gmail.com>
2024-08-22 09:41:30 +0700

    style invoice pdf

--- resources/views/orders/invoice.blade.php
+++ resources/views/orders/invoice.blade.php
@@ -1,255 +1,251 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('invoice.css') }}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
 <script src="https://kit.fontawesome.com/1b48e60650.js" crossorigin="anonymous"></script>
 
 <style type="text/css">
-*{
-    box-sizing: border-box;
-    font-size: 20px;
+
+:root {
+    --primary: #B1ADD4;
+    --font:#888;
 }
-body{
-    padding: 0;
+
+*, html {
     margin: 0;
-    overflow-x: hidden;
-    font-weight: normal;
-    font-weight: initial;
-    line-height: 1.5;
+    padding: 0;
+}
+
+body {
     font-family: "Rubik", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    color: #6c7293;
-    /* background-color: #191c24; */
 }
-.container{
-    max-width: 80%;
-    padding-right: 1rem;
-    padding-left: 1rem;
-    margin-right: auto;
-    margin-left: auto;
+
+.container {
+    padding: 30px;
 }
-.invoice{
-    background-color: #ffff;
-    border-radius: 5px;
-    color: #6c7293;
-    margin-top: 2rem;
-    .header{
-        display: flex;
-        justify-content: space-between;
-        padding: 1rem 0;
-        .name{
-            margin: auto 0;
-            padding-right: 1.5rem;
-            font-size: 2rem;
-            font-weight: 550;
-            span{
-                display: block;
-                text-align: end;
-            }
-        }
-    }
-    .info{
-        display: flex;
-        justify-content: space-between;
-        padding: 1rem;
-        /* width: 100%; */
-        span{
-            margin: 0 auto;
-        }
-    }
-    .customer{
-        margin-top: 0.5rem;
-        padding-bottom: 1rem;
-        .left{
-            display: flex;
-            align-content: center;
-            justify-content: space-between;
-            max-width: 30%;
-            margin-left: 1rem;
-            span{
-                display: block;
-            }
-            .body{
-                max-width: 75%;
-            }
-        }
-    }
-    .invoice_detail{
-        border-bottom: 2px dashed #B1ADD4;
-        .tabel_responsive{
-            margin: 1rem 0;
-            padding: 0 auto;
-        }
-        .table{
-            border-collapse: collapse;
-            thead th{
-                padding: 0.9375rem;
-                border-top: 0;
-                /* border-bottom-width: 1px; */
-                font-weight: 500;
-                color: #6c7293;
-                text-align: center;
-                border-bottom: 2px solid #6c7293;
-                vertical-align: bottom;
-            }
-            td{
-                vertical-align: middle;
-                font-size: 0.875rem;
-                line-height: 1;
-                white-space: nowrap;
-                padding: 0.9375rem;
-            }
-            tbody tr{
-                color: #6c7293;
-                text-align: center;
-                border-top: 0;
-                border-bottom-width: 1px;
-                border-bottom: 1.5px solid #6c7293;
-            }
-        }
-    }
-}
-.bottom_info{
-    border-bottom: 2px dashed #B1ADD4;
-    display: flex;
-    justify-content: space-between;
-    padding: 1rem 1.5rem;
-    .left{
-        .mb-2{
-            margin-bottom: 0.5rem;
-        }
-        span{
-            display: block;
-        }
-    }
-    .right{
-        display: flex;
-        justify-content: space-between;
-        width: 20%;
-        span{
-            display: block;
-        }
-    }
-}
-.text-center{
-    text-align: center;
-    padding: 1rem 0;
-    span.title{
+
+.title {
         font-size: 1.5rem;
-        font-weight: 500;
-    }
-    span{
-        display: block;
-    }
+        font-weight: 700;
+        text-align: right;
+        padding: 0 0 20px 0;
+        color: var(--font);
 }
+
+.top .table-head {
+    padding: 15px 0;
+    border-top: 1.4px dashed var(--primary);
+    border-bottom: 1.4px dashed var(--primary);
+    width: 100%;
+}
+
+.top .table-head h1 {
+    color: var(--font);
+    font-size: .9rem;
+    font-weight: 300
+    font-family: "Helvetica Neue";
+    text-align: center;
+}
+
+.top .table-content {
+    padding: 15px 0 15px 0;
+    font-size: .9rem;
+}
+
+.top .table-content .tc-head, .tc-content {
+    color: var(--font);
+}
+
+.top .table-content .tc-content .label {
+    padding: 0 30px 0 15px;
+}
+
+.bottom .table-head {
+    font-family: "Rubik", sans-serif;
+
+    border-top: 1.4px dashed var(--primary);
+    border-bottom: 1.4px dashed var(--primary);
+    width: 100%;
+    text-align: center;
+    padding: 10px 0;
+    font-size: .9rem;
+
+}
+.bottom .table-head thead tr td, .bottom .table-head tbody tr td {
+    font-size: .8rem;
+    height: 30px;
+    color: var(--font);
+}
+
+.bottom .table-head thead tr .label {
+    color: var(--font);
+    font-weight: 600;
+    padding-bottom: 5px;
+}
+
+.bottom .table-head tbody tr .value {
+    padding-top: 5px;
+}
+
+.bottom .table-head .border {
+    background: var(--font);
+    width: 1400%;
+    height: 1px;
+}
+
+.bottom .table-content {
+    padding: 10px 0;
+    width: 100%;
+    border-bottom: 1.4px dashed var(--primary);
+    font-size: 0.9rem;
+}
+
+.bottom .table-content tr {
+    padding: 0 0 13px 0;
+}
+
+.bottom .table-content tr td {
+    color: var(--font);
+}
+
+.bottom .table-content thead .wp-label-value-top {
+    padding: 0 0 7px 0;
+    width: 500px;
+}
+.bottom .table-content thead .label{
+    width: 130px;
+}
+.bottom .table-content thead .label{
+    padding: 0 0 7px 0;
+}
+.footer{
+    text-align: center;
+    width: 100%;
+    color: var(--font);
+}
+.footer .head{
+    margin-top: 15px;
+    display: block;
+    font-size: 1.5rem;
+    font-weight: 600;
+}
+.footer .mid{
+    font-size: 0.9rem;
+    display: block;
+}
+.footer .bot{
+    display: block;
+    font-size: 0.9rem;
+}
+
+
 </style>
 <div class="container invoice">
-    <div class="header">
-        <div class="logo">
-            {{-- <img src="{{ asset('logo.jpg') }}" alt="Logo" class="logo-img ml-4" style="width: 85px;height:85px"> --}}
-        </div>
-        <div class="name">
-            <span>INVOICE</span>
-        </div>
+    <h1 class="title">INVOICE</h1>
+    <div class="top">
+        <table class="table-head">
+            <tr>
+                <td><h1>Order ID : #{{ $order->order_id }}</h1></td>
+                <td><h1>Date : {{ now() }}</h1></td>
+            </tr>
+        </table>
+        <table class="table-content">
+            <thead class="tc-head">
+                <tr>
+                    <td>To :</td>
+                </tr>
+            </thead>
+            <tbody class="tc-content">
+                <tr>
+                    <td class="label">Name</td>
+                    <td class="value">: {{ $order->name }}</td>
+                </tr>
+                <tr>
+                    <td class="label">Email</td>
+                    <td class="value">: {{ $order->email }}</td>
+                </tr>
+                <tr>
+                    <td class="label">Phone</td>
+                    <td class="value">: {{ $order->phone }}</td>
+                </tr>
+                <tr>
+                    <td class="label">Address</td>
+                    <td class="value">: {{ $order->address }}{{ $order->city }} {{ $order->postal_code }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-    <div class="info" style="border-bottom: 2px dashed #B1ADD4;border-top: 2px dashed #B1ADD4">
-        <span>Order ID : #{{ $order->order_id }}</span>
-        <span>Date : {{ now() }}</span>
-    </div>
-    <div class="customer mt-2 pb-3" style="border-bottom: 2px dashed #B1ADD4;">
-        <span style="font-size: 1.2rem">To :</span>
-        <div class="left">
-            <div class="title">
-                <span>Name</span>
-                <span>Email</span>
-                <span>Phone</span>
-                <span>Address</span>
-            </div>
-            <div>
-                <span>:</span>
-                <span>:</span>
-                <span>:</span>
-                <span>:</span>
-            </div>
-            <div class="body">
-                <span>{{ $order->name }}</span>
-                <span>{{ $order->email }}</span>
-                <span>{{ $order->phone }}</span>
-                <span>
-                    {{ $order->address }}
-                    {{ $order->city }}
-                    {{ $order->postal_code }}
-                </span>
-            </div>
-        </div>
-    </div>
-    <div class="invoice_detail">
-        <div class="table-responsive tabel_responsive">
-            <table class="table" style="width: 100%">
-                <thead>
+    <div class="bottom">
+        <table class="table-head">
+            <thead>
+                <tr class="tr-top">
+                    <td class="label">No</td>
+                    <td class="label">Item</td>
+                    <td class="label">Category</td>
+                    <td class="label">Price</td>
+                    <td class="label">Qty</td>
+                    <td class="label">Amount</td>
+                </tr>
+            </thead>
+            <div class="border"></div>
+            <tbody>
+                @foreach ($order->orderItems as $index => $item)
                     <tr>
-                    <th style="font-weight:600;"> No </th>
-                    <th style="font-weight:600;"> Item </th>
-                    <th style="font-weight:600;"> Category </th>
-                    <th style="font-weight:600;"> Price </th>
-                    <th style="font-weight:600;"> Qty </th>
-                    <th style="font-weight:600;"> Amount </th>
+                        <td class="value">{{ $index + 1 }}</td>
+                        <td class="value">{{ $item->product_name }}</td>
+                        <td class="value">{{ $item->category_name }}</td>
+                        <td class="value">Rp. {{ number_format($item->price) }}</td>
+                        <td class="value">{{ $item->quantity }}</td>
+                        <td class="value">Rp. {{ number_format($item->price * $item->quantity) }}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    @foreach ($order->orderItems as $index => $item)
-                        <tr>
-                            <td>{{ $index + 1 }}</td>
-                            <td>{{ $item->product_name }}</td>
-                            <td>{{ $item->category_name }}</td>
-                            <td>Rp. {{ number_format($item->price) }}</td>
-                            <td>{{ $item->quantity }}</td>
-                            <td>Rp. {{ number_format($item->price * $item->quantity) }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                @endforeach
+            </tbody>
+        </table>
+        <table class="table-content">
+            <thead>
+                <tr>
+                    <td class="wp-label-value-top">
+                        <p>Waktu pemesanan</p>
+                    </td>
+                    <td class="label">Total Product</td>
+                    <td class="value">: {{ number_format($order->total, 0, ',', '.') }}</td>
+                </tr>
+                <tr>
+                    <td class="wp-label-value-top">
+                        <p>{{ $order->created_at }}</p>
+                    </td>
+                    <td class="label">Discon</td>
+                    <td class="value">: 0</td>
+                </tr>
+                <tr>
+                    <td class="wp-label-value-bot">
+                        <p>Waktu pemesanan</p>
+                    </td>
+                    <td class="label">Biaya Layanan</td>
+                    <td class="value">: 1.000</td>
+                </tr>
+                <tr>
+                    <td class="wp-label-value-bot">
+                        <p>{{ $order->updated_at }}</p>
+                    </td>
+                    <td class="label">Total Payment</td>
+                    <td class="value">: {{ number_format(($order->total  + 1000), 0, ',', '.') }}</td>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
     </div>
-    <div class="bottom_info">
-        <div class="left">
-            <div class="mb-2">
-                <span class="d-block">Waktu pemesanan</span>
-                <span>{{ $order->created_at }}</span>
-            </div>
-            <div>
-                <span class="d-block">Waktu pembayaran</span>
-                <span>{{ $order->updated_at }}</span>
-            </div>
-        </div>
-        <div class="right">
-            <div class="title">
-                <span>Total Product</span>
-                <span>Discon</span>
-                <span>Biaya Layanan</span>
-                <span>Total Payment</span>
-            </div>
-            <div class="middle">
-                <span>Rp.</span>
-                <span>Rp.</span>
-                <span>Rp.</span>
-                <span>Rp.</span>
-            </div>
-            <div class="body">
-                <span>{{ number_format($order->total, 0, ',', '.') }}</span>
-                <span>0</span>
-                <span>1.000</span>
-                <span>{{ number_format(($order->total  + 1000), 0, ',', '.') }}</span>
-            </div>
-        </div>
-    </div>
-    <div class="text-center">
-        <span class="title">
+    <div class="footer">
+        <span class="head">
             THANK YOU!..
         </span>
-        <span>
+        <span class="mid">
             Items that have been purchased cannot be returned
         </span>
-        <span>
+        <span class="bot">
             For more info contact support@gonemaul.my.id
         </span>
     </div>
